ec93b99b2438a5102280c1c347353b519f6935b4
krizah19 <50041864+krizah19@users.noreply.github.com>
2020-07-08 14:33:27 +0300

    new build

---
+++ client/build/precache-manifest.297573cbb6c9f401bb0cce2500ad618d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd4564369add70ffcb5d6c7a632f320",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4920e2b1be7b7b397951",
+    "url": "./static/css/2.cb7b4186.chunk.css"
+  },
+  {
+    "revision": "a0cd21c2488b48b993d7",
+    "url": "./static/css/main.39aaa0c4.chunk.css"
+  },
+  {
+    "revision": "4920e2b1be7b7b397951",
+    "url": "./static/js/2.1442b715.chunk.js"
+  },
+  {
+    "revision": "b97f918fc99d8a2ebbea2b6d6ec8ad4f",
+    "url": "./static/js/2.1442b715.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0cd21c2488b48b993d7",
+    "url": "./static/js/main.b9ff8c40.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  },
+  {
+    "revision": "af50d82ed8cb62e1e14556c66766a871",
+    "url": "./static/media/banner.af50d82e.jpg"
+  },
+  {
+    "revision": "e2424cb9f99e54fb1f8af6bf2c6e8ed2",
+    "url": "./static/media/banner0.e2424cb9.png"
+  },
+  {
+    "revision": "10225022c574ef1fbae9937548ba3ec7",
+    "url": "./static/media/banner1.10225022.png"
+  },
+  {
+    "revision": "bcee7932ac5d4c278bf615badf37cfd4",
+    "url": "./static/media/banner2.bcee7932.png"
+  },
+  {
+    "revision": "ac4e6aba4848e45a7bcc4d760b844b55",
+    "url": "./static/media/banner3.ac4e6aba.png"
+  },
+  {
+    "revision": "3a3d4ed61e995126540e0059e5282e44",
+    "url": "./static/media/banner4.3a3d4ed6.jpg"
+  },
+  {
+    "revision": "b739b5b2bfeaeceba82bcc2fb59c28af",
+    "url": "./static/media/banner5.b739b5b2.jpg"
+  },
+  {
+    "revision": "93221adaf62c197885ec6df568532aa2",
+    "url": "./static/media/banner6.93221ada.png"
+  },
+  {
+    "revision": "25fc5a9e00cae5a27158d3715903de1e",
+    "url": "./static/media/banner7.25fc5a9e.jpg"
+  },
+  {
+    "revision": "ba9ae1a381dbfb02858e593dc8237d76",
+    "url": "./static/media/banner8.ba9ae1a3.jpg"
+  },
+  {
+    "revision": "bd4790cbc9b58b029abd11f489f5d007",
+    "url": "./static/media/logo.bd4790cb.png"
+  }
+]);
